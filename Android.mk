df54e6ebe40aec29415460f07f874e78bbf6e94e
Cyril Wiggins <cyril.x9@gmail.com>
2016-04-24 21:51:48 -0400

    audio: Filter for msm8960 via TARGET_QCOM_AUDIO_VARIANT
    Change-Id: Iacee9f0c60ac7cfd266ffac75805fadf20a539d6

--- Android.mk
+++ Android.mk
@@ -1,5 +1,4 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
-
+ifeq ($(strip $(TARGET_QCOM_AUDIO_VARIANT)),caf-msm8960)
 ifneq ($(filter msm8960 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
@@ -11,5 +10,4 @@ include $(MY_LOCAL_PATH)/hal/Android.mk
 endif
 
 endif
-
 endif
